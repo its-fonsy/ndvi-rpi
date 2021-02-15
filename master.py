ec0cfdf56db0d0e5e17e2624df9e01b719522608
Fonsy <ciabadiale@gmail.com>
2021-02-15 10:31:29 +0100

    make a photo

--- master.py
+++ master.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+# Camera warm-up time
+sleep(2)
+camera.capture('foo.jpg')
+
