c4a9764b2434a955191779d773d962959db5570d
Fonsy <ciabadiale@gmail.com>
2021-02-15 12:14:18 +0100

    sync master and slave function

--- master.py
+++ master.py
@@ -7,11 +7,24 @@ RX = Button(2)
 TX = LED(3)
 TX.on()
 
-input("Press any key to send a signal")
-TX.off()
-sleep(1)
-TX.on()
-print("TX sent, waiting for response")
-while(not RX.is_pressed):
-    pass
-print("RX received!")
+def init_sync():
+    DELAY = 0.5
+    """
+    (MASTER)
+    Three step init_sync:
+    1. Receive RX from SLAVE
+    2. Send TX to SLAVE
+    3. Receive RX from SLAVE
+    """
+    print("Waiting for slave...")
+    while(RX.is_released):
+        pass
+    print("Got RX, ensuring is active")
+    TX.off()
+    sleep(DELAY)
+    TX.on()
+    while(RX.is_released):
+        pass
+    print("slave is active")
+
+init_sync()
--- slave.py
+++ slave.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python3
 
-from gpiozero import LED, Button
 from time import sleep
+from gpiozero import LED, Button
 
 RX = Button(2)
 TX = LED(3)
 TX.on()
 
-print("Waiting for RX")
-while(not RX.is_pressed):
-	pass
-print("RX received!")
+def init_sync():
+    DELAY = 0.5
+    """
+    (SLAVE)
+    Three step init_sync:
+    1. Send TX to MASTER
+    2. Receive RX to SLAVE
+    3. Send TX to MASTER
+    """
+    # Step 1
+    print("Sending TX to master")
+    TX.off()
+    sleep(DELAY)
+    TX.on()
 
-input("Press any key to send a signal")
-TX.off()
-sleep(1)
-TX.on()
+    # Step 2
+    while(RX.is_released):
+        pass
+
+    # Step 3
+    print("Got RX from master, confirm active slave")
+    TX.off()
+    sleep(DELAY)
+    TX.on()
+
+init_sync()
